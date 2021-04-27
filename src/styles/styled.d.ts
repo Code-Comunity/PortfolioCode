86bca5adfd2c6ca84f5f6baac00a267b7b584677
Vitor Saraiva <55057661+SaraivaVitor@users.noreply.github.com>
2021-04-26 23:26:12 -0300

    bug fix

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -5,5 +5,12 @@ import theme from './themes/theme'
 export type Theme = typeof theme
 
 declare module 'styled-components' {
-    export interface DefaultTheme extends theme {}
+    export interface DefaultTheme extends theme {
+    
+        colors: {   
+            background: string
+            text: string 
+            primary: string
+        }
+    }
 }
