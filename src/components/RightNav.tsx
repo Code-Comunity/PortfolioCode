4536447fae418294f2f68cdbbc336ea47000512e
Vitor Saraiva <55057661+SaraivaVitor@users.noreply.github.com>
2021-04-27 21:10:07 -0300

    Pequena correção de tipagem no menu responsivo

--- src/components/RightNav.tsx
+++ src/components/RightNav.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import React from 'react';
 import styled from 'styled-components';
 
-const Ul = styled.ul`
+const Ul = styled.ul<{open: boolean}>`
   list-style: none;
   display: none;
   flex-flow: row nowrap;
